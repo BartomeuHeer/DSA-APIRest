6ebeea5bd8bf7ae708c43079a285ca0e3dfe249f
BartomeuHeer <bheersalva@gmail.com>
2022-03-28 18:50:12 +0200

    first commit

---
+++ src/main/java/edu/upc/eetac/dsa/Conrtroller.java
@@ -0,0 +1,9 @@
+package edu.upc.eetac.dsa;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class Conrtroller {
+}
---
+++ src/main/java/edu/upc/eetac/dsa/SwaggerAPI.java
@@ -0,0 +1,8 @@
+package edu.upc.eetac.dsa;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+public interface SwaggerAPI {
+}
